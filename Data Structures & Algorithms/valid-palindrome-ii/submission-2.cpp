01c86f2333636f4ab79c1f7bb26f34423da6a9be
Sanjayj23 <sanjayj23@iitk.ac.in>
2026-08-13 17:32:29 +0530

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+        
+        while (i < j) {
+            if (s[i] != s[j]) {
+                return checkPalindrome(s, i + 1, j) || checkPalindrome(s, i, j - 1);
+            }
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+    
+private:
+    bool checkPalindrome(const string& s, int i, int j) {
+        while (i < j) {
+            if (s[i] != s[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
